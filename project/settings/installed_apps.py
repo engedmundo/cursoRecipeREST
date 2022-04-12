1193ec37dd8563da0ebf015718393e14f2bb9010
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-04-12 16:34:27 -0300

    first commit

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,15 @@
+INSTALLED_APPS = [
+    # django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # rest framework
+    'rest_framework',
+    # A gente tem apps também
+    'recipes',
+    'authors',
+    'tag',
+]
---
+++ recipes/views/api.py
@@ -0,0 +1,44 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from tag.models import Tag
+
+from ..models import Recipe
+from ..serializers import RecipeSerializer, TagSerializer
+
+
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 10
+
+
+class RecipeAPIv2ViewSet(ModelViewSet):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        print('Parâmetros', self.kwargs)
+        print('Query Strings', self.request.query_params)
+        return qs
+
+
+@api_view()
+def tag_api_detail(request, pk):
+    tag = get_object_or_404(
+        Tag.objects.all(),
+        pk=pk
+    )
+    serializer = TagSerializer(
+        instance=tag,
+        many=False,
+        context={'request': request}
+    )
+    return Response(serializer.data)
