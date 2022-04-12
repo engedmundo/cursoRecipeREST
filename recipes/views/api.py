88167b295c88348e1d1ad7e604adf86b2bfbe801
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-04-12 17:56:57 -0300

    criar permissões de usuário

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -8,7 +8,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from tag.models import Tag
-from rest_framework.permissions import IsAuthenticated
+from ..permissions import IsOwner
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
 
 from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
@@ -22,7 +24,8 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -31,6 +34,27 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         if category_id is not '' and category_id.isnumeric():
             qs = qs.filter(category_id=category_id)
         return qs
+
+    def get_object(self):
+        pk = self.kwargs.get('pk')
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+        return super().get_permissions()
+    
+    def list(self, request, *args, **kwargs):
+        print('REQUEST', request.user, self.request.user)
+        print(request.user.is_authenticated)
+        return super().list(request, *args, **kwargs)
+
 
 
 @api_view()
