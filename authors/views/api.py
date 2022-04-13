bb44d88a85532a1e45a8d421471075b8d55b2a39
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-04-13 11:50:59 -0300

    criar uma url personalizada

--- authors/views/api.py
+++ authors/views/api.py
@@ -1,6 +1,9 @@
+from re import A
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.decorators import action
 
 from ..serializers import AuthorSerializer
 
@@ -9,8 +12,18 @@ class AuthorViewSet(ReadOnlyModelViewSet):
     serializer_class = AuthorSerializer
     permission_classes = [IsAuthenticated, ]
 
-
     def get_queryset(self):
         User = get_user_model()
         queryset = User.objects.filter(username=self.request.user.username)
         return queryset
+    @action(
+        methods=['get'],
+        detail=False,
+    )
+    def me(self, request, *args, **kwargs):
+        obj = self.get_queryset().first()
+
+        serializer = self.get_serializer(
+            instance=obj,
+        )
+        return Response(serializer.data)
